4a5c7cc6e81a8a8ccba637f18c6b7c0f2cf2cfe7
miss_codein <219162820+VeeKreates@users.noreply.github.com>
2025-09-02 20:32:05 +0300

    Update script.js

---
+++ landing page/script.js
@@ -0,0 +1,20 @@
+// query selector gets the first element in the html paragraph that matches the css selector .
+// document.querySelectorAll(). selects all the elements
+// document.querySelector(CSS selectors)-syntax
+const wrap= document.querySelector('.wrap');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.registation-box');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+registerlink.addEventListener('click', ()=>{
+    wrap.classList.add('active');
+
+});
+loginlink.addEventListener('click', ()=>{
+    wrap.classList.remove('active');
+
+});
+btnPopup.addEventListener('click', ()=>{
+    wrap.classList.remove('active.btnPopup');
+
+});
